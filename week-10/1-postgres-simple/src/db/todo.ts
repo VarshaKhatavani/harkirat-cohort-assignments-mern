98f7753d50b2f1b3be9815f58d121be193bd9ebb
Varsha Khatavani <100074545+VarshaKhatavani@users.noreply.github.com>
2024-02-14 21:37:43 +0530

    Add files via upload

--- week-10/1-postgres-simple/src/db/todo.ts
+++ week-10/1-postgres-simple/src/db/todo.ts
@@ -1,3 +1,4 @@
+import { QueryResult } from "pg";
 import { client } from "..";
 /*
  * Function should insert a new todo for this user
@@ -9,8 +10,22 @@ import { client } from "..";
  *  id: number
  * }
  */
+
+interface Todo {
+    title: string,
+    description: string,
+    done: boolean,
+    id: number,
+    user_id:number
+}
+
 export async function createTodo(userId: number, title: string, description: string) {
-    
+        const query = "insert into todos(user_id,title,description) values($1,$2,$3)";
+        const values = [userId, title, description]
+        await client.query(query, values);
+        const t = await client.query("select * from todos order by id desc limit 1");
+        return t.rows[0];
+   
 }
 /*
  * mark done as true for this specific todo.
@@ -23,7 +38,18 @@ export async function createTodo(userId: number, title: string, description: str
  * }
  */
 export async function updateTodo(todoId: number) {
-
+    
+        let query = "select * from todos where id=$1";
+        const values = [todoId]
+        const todo = await client.query(query, values);
+        if (todo != null) {
+            query = "update todos set done=true where id=$1"
+            await client.query(query, values);
+        }
+        query = "select * from todos where id=$1";
+        const t = await client.query(query, values);
+        return t.rows[0];
+    
 }
 
 /*
@@ -37,5 +63,22 @@ export async function updateTodo(todoId: number) {
  * }]
  */
 export async function getTodos(userId: number) {
+    
+        const query = "select * from todos where user_id=$1";
+        const values = [userId]
+        const t= await client.query(query, values);
+        return  t.rows;
+        // let todos: Todo[] = [];
+        // t.rows.forEach(element => {
+        //     let todo: Todo = {
+        //         title: element.title,
+        //         description: element.description,
+        //         done: element.done == 0 ? false : true,
+        //         id: element.id
+        //     }
+        //     todos.push(todo);
+        // });
 
+        // return todos;
+    
 }
--- week-10/1-postgres-simple/src/db/user.ts
+++ week-10/1-postgres-simple/src/db/user.ts
@@ -9,7 +9,14 @@ import { client } from "..";
  *   name: string
  * }
  */
+
 export async function createUser(username: string, password: string, name: string) {
+    
+        const query = "insert into users(username,password,name) values($1,$2,$3)";
+        const values = [username, password, name]
+        await client.query(query, values);
+        const t = await client.query("select username,password,name from users order by id desc limit 1");
+        return t.rows[0];
     
 }
 
@@ -22,5 +29,10 @@ export async function createUser(username: string, password: string, name: strin
  * }
  */
 export async function getUser(userId: number) {
+    
+        const query = "select * from users where id=$1";
+        const values = [userId]
+        const t = await client.query(query, values);
+        return t.rows[0];
     
 }
